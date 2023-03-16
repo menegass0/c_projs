3750e90855103a5a9d69582757e17523295aeba7
menegass0 <victormenegasso@unisantos.br>
2023-03-15 21:01:35 -0300

    start-project1

---
+++ the_tech_machine/quiz_game/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+    int ans1, ans2, ans3, ans4, ans5;
+    int point1, point2, point3, point4, point5, point6, point7, point8, point9, point10;
+    int point01, point02, point03, point04, point05, point06, point07, point08, point09, point010;  
+
+    printf("Welcome to the Game \n\n");
+
+    printf("> Press 7 to start the game\n");
+    printf("> Press 8 to quit the game\n");
+
+    scanf("%d", &i);
+
+    if(i == 7){
+        printf("the game has started\n\n");
+        printf("1) Which is the first search engine in internet?\n\n");
+        printf("a) Google\n");
+        printf("b) archie\n");
+        printf("c) Wais\n");
+        printf("d) AltaVista\n");
+        
+        printf("Enter your answer: ");
+        scanf("%d", &ans1);
+
+        if(ans1 == )
+    }
+    else if(i == 8){
+        printf("the game has ended\n\n");
+    }
+    else{
+        printf("Invalid\n\n");
+    }
+
+
+    return 0;
+}
