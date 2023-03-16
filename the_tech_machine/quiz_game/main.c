4931f77ab498ea70424a647c22fa401f80b44e10
menegass0 <victormenegasso@unisantos.br>
2023-03-15 22:16:03 -0300

    fim do projeto original

--- the_tech_machine/quiz_game/main.c
+++ the_tech_machine/quiz_game/main.c
@@ -3,8 +3,8 @@
 int main(){
     int i;
     int ans1, ans2, ans3, ans4, ans5;
-    int point1, point2, point3, point4, point5, point6, point7, point8, point9, point10;
-    int point01, point02, point03, point04, point05, point06, point07, point08, point09, point010;  
+    int point1, point2, point3, point4, point5;
+    int point01, point02, point03, point04, point05;  
 
     printf("Welcome to the Game \n\n");
 
@@ -16,15 +16,107 @@ int main(){
     if(i == 7){
         printf("the game has started\n\n");
         printf("1) Which is the first search engine in internet?\n\n");
-        printf("a) Google\n");
-        printf("b) archie\n");
-        printf("c) Wais\n");
-        printf("d) AltaVista\n");
+        printf("1) Google\n");
+        printf("2) archie\n");
+        printf("3) Wais\n");
+        printf("4) AltaVista\n");
         
         printf("Enter your answer: ");
         scanf("%d", &ans1);
 
-        if(ans1 == )
+        if(ans1 == 2){
+            printf("Correct Answer\n");
+            point1 = 5;
+            printf("You have scored %d points\n\n", point1);
+        }
+        else {
+            printf("Wrong answer\n");
+            point01 = 0;
+            printf("You have scored %d point\n\n", point01);
+        }
+
+        printf("2) Which one is the first web browser inveted in 1990?\n\n");
+        printf("1) Internet Explorer\n");
+        printf("2) Mosaic\n");
+        printf("3) Mozilla\n");
+        printf("4) Nexus\n");
+
+        printf("Enter your answer: ");
+        scanf("%d", &ans2);
+        
+        if(ans2 == 4){
+            printf("Correct Answer\n");
+            point2 = 5;
+            printf("you have scored %d points\n\n", point2);
+        }
+        else{
+            printf("Wrong answer\n");
+            point02 = 0;
+            printf("You have scored %d points\n\n", point02);
+        }
+
+        printf("3) First computer virus is know as?\n\n");
+        printf("1) Rabbit\n");
+        printf("2) Creeper Virus\n");
+        printf("3) Elk cloner \n");
+        printf("4) SCA Virus\n");
+
+        printf("Enter your answer: ");
+        scanf("%d", &ans3);
+
+        if(ans3 == 2){
+            printf("Correct Answer\n");
+            point3 = 5;
+            printf("you have scored %d points\n\n", point3);
+        }
+        else{
+            printf("Wrong answer\n");
+            point03 = 0;
+            printf("You have scored %d points\n\n", point03);
+        }
+
+        printf("4) Firewakk in computer is used for?\n\n");
+        printf("1) Security\n");
+        printf("2) Data Transmission\n");
+        printf("3) Monitoring\n");
+        printf("4) Authentication\n");
+
+        printf("Enter your answer: ");
+        scanf("%d", &ans4);
+
+        if(ans4 == 1){
+            printf("Correct Answer\n");
+            point4 = 5;
+            printf("you have scored %d points\n\n", point4);
+        }
+        else{
+            printf("Wrong answer\n");
+            point04 = 0;
+            printf("You have scored %d points\n\n", point04);
+        }
+
+        printf("5) Which of the following is not a database management software?\n\n");
+        printf("1) Mysql\n");
+        printf("2) Oracle\n");
+        printf("3) cobal\n");
+        printf("4) Sybase\n");
+
+        printf("Enter your answer: ");
+        scanf("%d", &ans5);
+
+        if(ans5 == 3){
+            printf("Correct Answer\n");
+            point5 = 5;
+            printf("you have scored %d points\n\n", point4);
+        }
+        else{
+            printf("Wrong answer\n");
+            point05 = 0;
+            printf("You have scored %d points\n\n", point05);
+        }
+
+        printf("the total of points is: %d", (point1+point2+point3+point4+point5));
+
     }
     else if(i == 8){
         printf("the game has ended\n\n");
